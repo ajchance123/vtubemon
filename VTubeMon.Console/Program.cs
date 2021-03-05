93857e7cd587ff21e511ed40ad46a49e1edfaa87
Austin Chance <ajchance123@gmail.om>
2021-03-04 16:52:03 -0800

    Adding a UWP app incase we want a GUI. Also added the boilerplate code for the discord client connection.

--- VTubeMon.Console/Program.cs
+++ VTubeMon.Console/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
 
 namespace VTubeMon.Console
 {
@@ -7,6 +9,17 @@ namespace VTubeMon.Console
         static void Main(string[] args)
         {
             System.Console.WriteLine("VTubers rock");
+        }
+
+        public static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "ODE3MTQ3NTIwNzM1NjQxNjQw.YEFR7g.7xdD_sWWLUoD3EmlLzhdFX61KSQ",
+                TokenType = TokenType.Bot
+            });
+
+            await discord.ConnectAsync();
         }
     }
 }
---
+++ VTubeMon.UApp/MainPageViewModel.cs
@@ -0,0 +1,87 @@
+﻿using DSharpPlus;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+
+namespace VTubeMon.UApp
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel()
+        {
+            //TODO move this logic out of the view model
+
+            _client = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "ODE3MTQ3NTIwNzM1NjQxNjQw.YEFR7g.7xdD_sWWLUoD3EmlLzhdFX61KSQ",
+                TokenType = TokenType.Bot
+            });
+
+            MessageCollection = new ObservableCollection<string>();
+
+            _client.MessageCreated += Client_MessageCreated;
+            _uiTimer = new DispatcherTimer();
+            _uiTimer.Interval = TimeSpan.FromSeconds(1);
+            _uiTimer.Tick += UpdatePing;
+        }
+
+        private void UpdatePing(object sender, object e)
+        {
+            Ping = _client.Ping;
+        }
+
+        private DiscordClient _client;
+        private DispatcherTimer _uiTimer;
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        private async Task Client_MessageCreated(DSharpPlus.EventArgs.MessageCreateEventArgs e)
+        {
+            MessageCollection.Add(e.Message.Content);
+        }
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+
+        public ICollection<string> MessageCollection { get; }
+        public ICommand ConnectCommand => new RelayCommand(Connect, true);
+        public ICommand DisconnectCommand => new RelayCommand(Disconnect, true);
+
+        private bool _canConnect = true;
+        public bool CanConnect
+        {
+            get => _canConnect;
+            set => Set(ref _canConnect, value);
+        }
+        private async void Connect()
+        {
+            await _client.ConnectAsync();
+            CanConnect = false;
+            CanDisconnect = true;
+            _uiTimer.Start();
+        }
+
+        private bool _canDisconnect = false;
+        public bool CanDisconnect
+        {
+            get => _canDisconnect;
+            set => Set(ref _canDisconnect, value);
+        }
+        private async void Disconnect()
+        {
+            await _client.DisconnectAsync();
+            CanConnect = true;
+            CanDisconnect = false;
+            _uiTimer.Stop();
+        }
+
+        private int _ping;
+        public int Ping
+        {
+            get => _ping;
+            set => Set(ref _ping, value);
+        }
+    }
+}
